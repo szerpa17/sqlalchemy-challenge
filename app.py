8400b8603a3339c2236ae99f82cc7f58b8be8ad4
szerp <szerp002@gmail.com>
2020-08-16 21:46:50 -0400

    updated flask comments and code

--- app.py
+++ app.py
@@ -37,81 +37,85 @@ app = Flask(__name__)
 @app.route("/")
 def HomePage():
     """Home Page"""
-    # session.close
-    return ("Welcome to Hawaii Climate Analysis API <br/>"
-            "Available Routes:<br/>"
-            "Precipitation: /api/v1.0/precipitation <br/>"
-            "Stations: /api/v1.0/stations <br/>"
-            "Temperature Observations: /api/v1.0/tobs <br/>"
-            "Min, AVG, Max Temperature: /api/v1.0/<start> and /api/v1.0/<start>/<end>")
+    home_page_text = ("<h2>Welcome to Hawaii Climate Analysis API</h2>"
+                    "<strong>Available Routes:</strong>"
+                    "<li>Precipitation data (all available): /api/v1.0/precipitation </li>"
+                    "<li>Stations: /api/v1.0/stations </li>"
+                    "<li>Temperature observations (for the most active station/last year available): /api/v1.0/tobs </li>"
+                    "<li>Single day - Min, Average and Max temperature observations: /api/v1.0/&#60;start&#62; </li>"
+                    "<li>Multi day - Min, Average and Max temperature observations: /api/v1.0/&#60;start&#62;/&#60;end&#62; </li>")
+
+    return home_page_text
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    """Return a JSON list of precipitation."""
+
     session = Session(engine)
+    
+    # Retrieve the date and precipitation scores in database
+    results = (session.query(measurement.date, measurement.prcp).
+            filter(measurement.prcp != None).
+            all())
+
+    # Build dictionary
+    precip = {date: prcp for date, prcp in results}
+    # Jsonify result
+    jsonified_precip = jsonify(precip)
+
+    session.close
+    return (jsonified_precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a JSON list of stations from the dataset."""
+    
+    session = Session(engine)
+    
+    # Retrieve all stations
+    station_results = (session.query(station.name, station.station).
+                group_by(station.name).
+                all())
+    
+    # Build dictionary
+    station_dict = {station_id: name for name, station_id in station_results}
+    # Jsonify result
+    jsonified_stations = jsonify(station_dict ) 
+    
+    session.close
+    return (jsonified_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Query the dates and temperature observations (tobs) of 
+    the most active station for the last year of data."""
+    
+    session = Session(engine)
+
+    # Identify the max date in the database
     last_date = session.query(func.max(measurement.date)).all()[0][0]
-    #print(f'The max date in the dataset is: {last_date}, its type is {type(last_date)} \n')
 
     # Convert date to datetime format
     dt_max_date = dt.datetime.strptime(last_date, '%Y-%m-%d')
 
     # Calculate the date 1 year prior to last date in database
     date_year_prior = dt_max_date - dt.timedelta(days=365)
-    #print(f'365 days prior to last date in dataset: {year_prior.date()} \n')
 
-    # Perform a query to retrieve the data and precipitation scores
-    results = (session.query(measurement.date, measurement.prcp).
-            filter(measurement.date>=date_year_prior).
-            filter(measurement.prcp != None).
-            all())
-
-    precip = {date: prcp for date, prcp in results}
-    jsonified_precip = jsonify(precip)
-
-    session.close
-
-    return (
-        
-        jsonified_precip
- 
-    )
-
-@app.route("/api/v1.0/stations")
-def stations():
-    session = Session(engine)
-    station_results = (session.query(station.name, station.station).
-                group_by(station.name).
-                all())
-    
-    station_dict = {station_id: name for name, station_id in station_results}
-    jsonified_stations = jsonify(station_dict ) 
-    
-    session.close
-
-    return (
-        
-        jsonified_stations
- 
-    )
-
-@app.route("/api/v1.0/tobs")
-def tobs():
-    session = Session(engine)
-
+     # Retrieve tobs data
     station_temp_results = (session.query(measurement.station, measurement.date, measurement.tobs).
                     filter(station.station == 'USC00519281').
                     filter(measurement.tobs != None).
+                    filter(measurement.date>=date_year_prior).
                     all())
+    
+    # Build dictionary
     station_temp_dict = {date: tobs for station, date, tobs in station_temp_results}
+    # Jsonify result
     jsonified_tobs = jsonify(station_temp_dict) 
 
     session.close
-
-    return (
-        
-        jsonified_tobs
- 
-    )
+    return (jsonified_tobs)
 
 @app.route("/api/v1.0/<start>")
 def start_date_data(start):
@@ -122,6 +126,7 @@ def start_date_data(start):
     session = Session(engine)
     # Identify min and max dates in database
     dates = session.query(func.min(measurement.date),func.max(measurement.date)).all()[0]
+    
     # Function to populate temp observation min, max, avg and max values
     sel = [measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
     single_date_return = (session.query(*sel).filter(func.strftime("%Y-%m-%d", measurement.date) == start).all())
@@ -135,18 +140,16 @@ def start_date_data(start):
             null_vals = jsonify({"error class": 404, 
                                  "error": f"Date input {start} is not available in the database.",
                                  "guidelines": {"format": "Correct query format is YYYY-MM-dd", 
-                                                "range": f"Dates should range between {dates[0]} and {dates[1]}"},
-                                 "missing dates": "If requested input meets guidelines, it is possible there was no data collected for the inputted day"})
-            return null_vals
+                                                "range": f"Date should fall between {dates[0]} and {dates[1]}"}})
+            return null_vals, 404
     # If date is not null, input information into a dictionary
         else:
             station_temp_dict[row[0]] =  {'min temp':row[1], 'avg temp': row[2], 'max temp':row[3]}
     
     # Jsonify result
     jsonified_single_date_return = jsonify(station_temp_dict)
-    session.close
     
-    # Return jsonified dictionary
+    session.close
     return (jsonified_single_date_return)
 
 @app.route("/api/v1.0/<start>/<end>")
@@ -164,19 +167,21 @@ def start_and_end_date_data(start, end):
     # Verify if input can be converted to datetime
         start_date = dt.datetime.strptime(start, "%Y-%m-%d")
         end_date = dt.datetime.strptime(end, "%Y-%m-%d")
+
     except (TypeError, ValueError):
         session.close
         format_error = jsonify({"error class": 404, 
                             "error": f"Date input {start}/{end} is not readable.",
-                            "recommendation": {"format": 'Correct query format is "YYYY-MM-dd"/"YYYY-MM-dd"'}})
-        return format_error
+                            "recommendation": {"format": 'Correct query format is YYYY-MM-dd/YYYY-MM-dd'}})
+        return format_error, 404
 
     # Verify if user input dates fall within data range
     if start < dates[0] or end > dates[1]:
         date_range_error = jsonify({"error class": 404, 
                             "error": f'Date input "{start}"/"{end}"" is not available in the database range',
                             "range": f"Dates should range between {dates[0]} and {dates[1]}"})
-        return date_range_error 
+        return date_range_error, 404
+
     else:
         delta = end_date - start_date
 
